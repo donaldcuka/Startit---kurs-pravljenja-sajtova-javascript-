6ed75344587e465f74aba5b6a4a7d02798ea7ef7
donaldcuka <donaldcuka@gmail.com>
2017-03-03 20:19:40 +0100

    Zamena 4 za 25 y nizu
    domaci zadatak br 1 - 
    4. cas

---
+++ zamena 4 - 25/script.js
@@ -0,0 +1,15 @@
+ var niz = [7, 4, 52, 10, 4, 5, 12, 1, 3, 6];
+ var trazenaVrednost = 4;
+ var vrednostZaZamenu = 25;
+ var noviNiz = [];
+ function nesto(){
+ for ( var i = 0; i < niz.length; i++){
+ 	if (niz[i] == 4){
+ 		niz[i] = 25;
+
+ 	}
+ 		 	noviNiz.push(niz[i]);}
+
+ 	console.log(noviNiz);
+ }
+ // nesto();
